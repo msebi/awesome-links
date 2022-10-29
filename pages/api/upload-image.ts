9ab3b1348a43d402480c96565b54d03e3f9fbe45
msebi <28646632+msebi@users.noreply.github.com>
2022-10-30 00:54:40 +0200

    Course 5: trigger vercel

--- pages/api/upload-image.ts
+++ pages/api/upload-image.ts
@@ -2,6 +2,7 @@ import aws from 'aws-sdk'
 
 const handler = async (req, res) => {
     try {
+        // TODO: rem. This line was added to trigger deployment on vercel
         // 1.
         const s3 = new aws.S3({
             accessKeyId: process.env.PROJECT_AWS_ACCESS_KEY,
