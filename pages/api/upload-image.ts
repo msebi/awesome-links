21dc95dc05e17fb71cdc4a285e077dd24dc7fa1b
msebi <28646632+msebi@users.noreply.github.com>
2022-11-05 02:20:19 +0100

    part-5: show logs in deployment

--- pages/api/upload-image.ts
+++ pages/api/upload-image.ts
@@ -3,6 +3,7 @@ import aws from 'aws-sdk'
 const handler = async (req, res) => {
     try {
         // TODO: rem. Trigger rebuild 2
+        console.log('INF shows on vercel?')
         // 1.
         const s3 = new aws.S3({
             accessKeyId: process.env.PROJECT_AWS_ACCESS_KEY,
